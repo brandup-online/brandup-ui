96fb9b3a25193c1ecfdc2742c387ab80a926fbc9
Dmitry <d.kovyazin@hotmail.com>
2019-11-12 12:54:18 +0700

    Fix bug.

--- brandup-ui/src/element.ts
+++ brandup-ui/src/element.ts
@@ -165,16 +165,16 @@ window.addEventListener("click", commandClickHandler, false);
 (function () {
     if (typeof window["CustomEvent"] === "function") return false; //If not IE
 
-    let CustomEvent = function(event, params) {
+    let customEvent = function(event, params) {
         params = params || { bubbles: false, cancelable: false, detail: undefined };
         var evt = document.createEvent('CustomEvent');
         evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
-        return evt;
+        return <any>evt;
     }
 
-    CustomEvent.prototype = window["Event"].prototype;
+    customEvent.prototype = window["Event"].prototype;
 
-    window["CustomEvent"] = CustomEvent;
+    (<any>window)["CustomEvent"] = customEvent;
 })();
 
 export interface IEventOptions {
