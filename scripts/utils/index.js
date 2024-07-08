27c5b03da030607f7d9f0cffa787b62d76c8f082
Dmitry Kovyazin <it@brandup.online>
2024-07-08 21:27:33 +0700

    use @brandup/autonpm

--- scripts/utils/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const getPackageJson = require( './getpackagejson.js' );
-const setPackageJson = require( './setpackagejson.js' );
-
-module.exports = {
-	getPackageJson,
-	setPackageJson
-};
