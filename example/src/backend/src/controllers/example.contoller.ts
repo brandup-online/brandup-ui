893e48103bbeceea3246506230ba251a2a637c22
Mikhail-Nazaro <misha-nazarov.nazarov@yandex.ru>
2024-07-16 09:22:10 +0700

    fix redirect

--- example/src/backend/src/controllers/example.contoller.ts
+++ example/src/backend/src/controllers/example.contoller.ts
@@ -4,12 +4,10 @@ import { distDir } from "..";
 
 export default class ExampleContoller {
     getHTMLTemplate(req: Request, res: Response): void {
-        console.log('getHTMLTemplate')
         return res.sendFile(path.join("index.html"), { root: distDir });
     }
 
     redirectForms(req: Request, res: Response): void {
-        console.log('redirectForms')
         res.redirect("/forms");
     }
 }
