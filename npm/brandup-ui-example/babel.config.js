cdcfefc836484aefafecfe9beaf1b6c37abeea80
Dmitry Kovyazin <it@brandup.online>
2025-07-19 18:07:54 +0700

    refactorign brandup-ui-example

--- npm/brandup-ui-example/babel.config.js
+++ npm/brandup-ui-example/babel.config.js
@@ -9,15 +9,13 @@ const plugins = [
 	]
 ]; // '@babel/plugin-transform-runtime'
 
-const isModern = process.env.BROWSERS_ENV === 'modern';
-
 module.exports = {
   presets: [
     [
 		"@babel/preset-env", {
 			useBuiltIns: "usage",
 			corejs: "3.37.1",
-			targets: isModern ? { esmodules: true } : undefined,
+			targets: undefined,
 			debug: false
     	}
 	],
